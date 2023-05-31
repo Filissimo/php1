cc6e81a48a0281ab12e5dfb858ced1f1d4b85aae
Filips Matvejevs <filissimo@gmail.com>
2023-05-31 16:30:46 +0300

    false index

--- file7.php
+++ file7.php
@@ -1,18 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <script src="https://code.jquery.com/jquery-3.7.0.min.js"></script>
-    <title>Document</title>
-</head>
-<style>
-    #regresult, #logresult {
-        color: red
-    }
-</style>
-<body>
+
     <?php
     include 'file7/session.php';
     include 'file7/log_reg.php';
